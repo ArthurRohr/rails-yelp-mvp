e605404c25f414e97fcfda72e26db088b72485cb
arthur rohr <arthur.rohr.pcc@gmail.com>
2023-08-16 11:54:36 +0200

    rails new with models, migrate, validations and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+# Seed your restaurant database in db/seeds.rb with at least 5 valid restaurant records.
+require 'faker'
+
+# Clear existing data
+Restaurant.destroy_all
+
+# Create valid restaurant records using Faker
+puts "Creating new restaurants..."
+20.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.street_address
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+
+  Restaurant.create!(
+    name: name,
+    address: address,
+    category: category
+  )
+end
+
+puts "Seeded restaurants using Faker"
